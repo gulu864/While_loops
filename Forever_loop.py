aef4422ee0210b8f2f6f4d4a654a802727bdd079
gulu864 <suva.kanta4u@gmail.com>
2024-10-24 18:55:50 +0530

    Add files via upload

---
+++ Forever_loop.py
@@ -0,0 +1,9 @@
+i=0
+while True:
+ answer=input("Do you want to continue? Yes/No:")
+ if answer == "Yes":
+  
+   print("I WILL RUN FOREVER")
+ else:
+   print("Thank you")
+   break
---
+++ While_loops.py
@@ -0,0 +1,10 @@
+x=int(input("Enter a number:"))
+
+sum=0
+y=1
+
+while y<=x:
+ sum = sum + y
+ y = y + 1
+
+print("\nSum =",sum)
